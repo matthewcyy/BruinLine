dd12564c5fb9c1af73ff798de02aaf326fcb173f
steph <70790806+stephucla@users.noreply.github.com>
2021-11-02 21:26:44 -0700

    added react router, and moved everything to index.js and index.css

--- bl-client/src/index.js
+++ bl-client/src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <header className="App-header">
+        <div className="App-menu">
+          <button className="menu-item">
+            <h1> BOO </h1>
+          </button>
+          <button className="menu-item">
+            <h1> BOO </h1>
+          </button>
+          <button className="menu-item">
+            <h1> BOO </h1>
+          </button>
+          <button className="menu-item">
+            <h1> BOO </h1>
+          </button>
+        </div>
+        <p> HELLO </p>
+      </header>
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
