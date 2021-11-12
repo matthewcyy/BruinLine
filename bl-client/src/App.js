230a4e744bcc8cadec477e13737f22de9fc00503
steph <70790806+stephucla@users.noreply.github.com>
2021-11-11 16:39:27 -0800

    Add Basic Nav Bar
    Basic navigation bar was added to App.js with corresponding css.
    Needs to be changes to include login check for certain buttons.

--- bl-client/src/App.js
+++ bl-client/src/App.js
@@ -1,5 +1,17 @@
+<<<<<<< Updated upstream
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+=======
+import React, {useState, useEffect } from 'react';
+import { BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import axios from 'axios';
+import Home from './components/Dashboard/Home/Home';
+import Profile from './components/Dashboard/Profile/Profile';
+import Rankings from './components/Dashboard/Rankings/Rankings';
+import DiningHalls from './components/Dashboard/DiningHalls/DiningHalls';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+>>>>>>> Stashed changes
 
 function Home() {
   return <h2> HOME </h2>;
@@ -19,6 +31,7 @@ function Profile() {
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <Router>
       <div className="App-menu">
         <nav>
@@ -37,6 +50,47 @@ function App() {
             </li>
           </ul>
         </nav>
+=======
+    <BrowserRouter>
+    <div className="nav">
+      <Link to="/">
+        <button type="button" className="button1">
+          Home
+        </button>
+      </Link>
+      
+      <Link to="/register">
+      <button type="button" className="button1">
+      Register
+      </button>
+      </Link> 
+      <Link to="/login">
+      <button type="button" className="button1">
+      Login
+      </button>
+      </Link>
+      <Link to="/dininghalls">
+      <button type="button" className="button1">
+      Dining Halls
+      </button>
+      </Link>
+      <Link to="/home">
+      <button type="button" className="button1">
+      Home
+      </button>
+      </Link>
+      <Link to="/profile">
+      <button type="button" className="button1">
+      Profile
+      </button>
+      </Link>
+      <Link to="/rankings">
+      <button type="button" className="button1">
+      Rankings
+      </button>
+      </Link>
+    </div>
+>>>>>>> Stashed changes
         <Switch>
           <Route path="/DiningHalls">
             <DiningHalls />
---
+++ bl-client/src/components/Dashboard/Home/Home.js
@@ -0,0 +1,14 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div>
+    <div className="header">
+      <h2> HOME </h2>
+    </div>
+    <div className="nav">
+    </div>
+    </div>
+  );
+}
+export default Home;
