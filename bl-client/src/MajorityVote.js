04941d0a3382b835ffab9ff251f7fc92b51e678a
newtyee18 <92614955+newtyee18@users.noreply.github.com>
2021-11-11 16:48:36 -0800

    Changed MajorityVote to consider dynamic data
    Used a template from canvasjs.com for plotting dynamic data into a bar chart.
    The template has a different purpose and thus I have begun making changes to
    fit the goals of MajorityVote. I have currently changed the title and what
    each bar represents (the name of the dining halls). Currently, the
    premade code uses an automatic function that changes the values of the
    bars. I need to work on changing the dynamic data to keep track button presses
    that represent people clicking on the button to choose where they are going. The
    chart also needs to reset during certain periods of the day to indicate
    which meal hour it is (breakfast, lunch, dinner).

--- bl-client/src/MajorityVote.js
+++ bl-client/src/MajorityVote.js
@@ -1,37 +1,65 @@
-//got template code to make a column chart from: https://canvasjs.com/react-charts/column-chart/
+//got template code to make a bar chart from: https://canvasjs.com/react-charts/dynamic-live-column-chart/
 var React = require('react');
 var Component = React.Component;
 var CanvasJSReact = require('./canvasjs.react');
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+var updateInterval = 500;
 class MajorityVote extends Component {
+	constructor() {
+		super();
+		this.updateChart = this.updateChart.bind(this);
+	}
+	componentDidMount(){
+		setInterval(this.updateChart, updateInterval);
+	}
+	updateChart() {
+		var dpsColor, dpsTotal = 0, deltaY, yVal;
+		var dps = this.chart.options.data[0].dataPoints;
+		var chart = this.chart;
+		for (var i = 0; i < dps.length; i++) {
+			deltaY = Math.round(2 + Math.random() *(-2-2));
+			yVal = deltaY + dps[i].y > 0 ? (deltaY + dps[i].y < 100 ? dps[i].y + deltaY : 100) : 0;
+			dpsColor = yVal >= 90 ? "#e40000" : yVal >= 70 ? "#ec7426" : yVal >= 50 ? "#81c2ea" : "#88df86 ";
+			dps[i] = {label: "Core "+(i+1) , y: yVal, color: dpsColor};
+			dpsTotal += yVal;
+		}
+		chart.options.data[0].dataPoints = dps;
+		chart.render();
+	}
 	render() {
 		const options = {
+			theme: "dark2",
 			title: {
-				text: "Basic Column Chart"
+				text: "Dining Hall Majority Vote"
 			},
-			data: [
-			{
-				// 
+			subtitles: [{
+				text: "Where are we going to eat?"
+			}],
+			axisY: {
+				title: "Total Votes",
+				includeZero: true,
+			maximum: 100
+			},
+			data: [{
 				type: "column",
+				yValueFormatString: "#,###'%'",
+				indexLabel: "{y}",
 				dataPoints: [
-					{ label: "De Neve",  y: 10  },
-					{ label: "Bruin Plate", y: 15  },
-					{ label: "Bruin Cafe", y: 25  },
-					{ label: "Bruin Bowl",  y: 30  },
-					{ label: "Spice Kitchen at Feast",  y: 28  }
+					{ label: "Epicuria", y: 0 },
+					{ label: "Bruin Plate", y: 0 },
+					{ label: "De Neve", y: 0 }
 				]
-			}
-			]
+			}]
 		}
 		return (
-		<div>
-			<CanvasJSChart options = {options}
-				/* onRef={ref => this.chart = ref} */
-			/>
-			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-		</div>
+			<div>
+				<CanvasJSChart options = {options}
+					 onRef={ref => this.chart = ref}
+				/>
+				{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+			</div>
 		);
 	}
 }
-module.exports = MajorityVote;    
+module.exports = MajorityVote; 
