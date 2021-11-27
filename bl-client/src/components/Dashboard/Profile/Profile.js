1894c7b525bf5d14390e185fb955eefc021db123
Charles Bucquet <charles@bucquet.com>
2021-11-27 12:11:09 -0500

    Started to work on the Profile
    Implemented very basic layout (Username, email, password, favorite foods, etc...)

--- bl-client/src/components/Dashboard/Profile/Profile.js
+++ bl-client/src/components/Dashboard/Profile/Profile.js
@@ -1,4 +1,38 @@
+import React from 'react';
+import './Profile.css';
+import logo from '../../../images/BLINE LOGO OUTLINED.png';
+
+
+
 function Profile() {
-    return <h2> PROFILE </h2>;
+
+
+
+    return (
+        <div class="container">
+            <h2 class="title">Profile</h2>
+            <div class="ProfilePictureContainer">
+                <img src={logo} class="profilePicture" />
+            </div>
+            <div class="infoBoard">
+                <div class="subInfo">
+                    <p>Username:</p>
+                    <p>Email:</p>
+                    <p>Password:</p>
+                    <div class="groups">
+                        <p>Groups</p>
+                    </div>
+                </div>
+                <div class="subInfo">
+                    <div class="foods">
+                        <p>Favorite foods:</p>
+                    </div>
+                    <div class="invitations">
+                        <p>Invitations</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 export default Profile;
