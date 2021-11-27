6ae6bf1b4c3303106a7000adb8191f89d1431af7
Charles Bucquet <charles@bucquet.com>
2021-11-27 14:19:22 -0500

    Implemented some Profile features on the page
    The page now displays some dymamic data: email, username, favorite foods,
    groups, and invites. The user can also change passwords if needed

--- bl-client/src/components/Dashboard/Profile/Profile.js
+++ bl-client/src/components/Dashboard/Profile/Profile.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './Profile.css';
 import logo from '../../../images/BLINE LOGO OUTLINED.png';
+import axios from 'axios';
 
 
 
 function Profile() {
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [groups, setGroups] = useState([]);
+    const [favorites, setFavorites]  = useState([]); // Do the same for other data
+    const [invitations, setInvitations] = useState([]);
+    const [id, setId] = useState();
+    const [error, setError] = useState();
 
+    useEffect(() => {
+        async function getUserData() {
+            let token = localStorage.getItem("auth-token");
+            const userRes = await axios.get("http://localhost:5000/users/", {
+                headers: { "x-auth-token": token },
+            });
+            setUsername(userRes.data.username);
+            setEmail(userRes.data.email);
+            setGroups(userRes.data.groups);
+            setFavorites(userRes.data.favFoods);
+            setInvitations(userRes.data.invitations);
+            setId(userRes.data.id);
+        }
+
+        getUserData(); 
+    }, []);
+
+    const submit = async () => {
+        try{
+            const requestBody = {password, id};
+            await axios.post("http://localhost:5000/users/changePassword", requestBody);
+        } catch(err) {
+            err.response.data.msg && setError(err.response.data.msg)
+        }
+    };
 
 
     return (
@@ -16,19 +50,43 @@ function Profile() {
             </div>
             <div class="infoBoard">
                 <div class="subInfo">
-                    <p>Username:</p>
-                    <p>Email:</p>
-                    <p>Password:</p>
+                    <p>Username: {username}</p>
+                    <p>Email: {email}</p>
+                    <div class="password">
+                        <input type="password" onChange={obj => setPassword(obj.target.value)}/>
+                        <button onClick={submit}>Update Password</button>
+                        {/* <Button>Update Password</Button> */}
+                    </div>
+                    <br />
                     <div class="groups">
-                        <p>Groups</p>
+                        <p>Groups:</p>
+                        <ul>
+                            {groups.map(group => {
+                                return <li>{group.groupName}</li>
+                            })}
+                        </ul>
                     </div>
                 </div>
                 <div class="subInfo">
                     <div class="foods">
                         <p>Favorite foods:</p>
+                        <ul>
+                            {favorites.map(foodName => {
+                                if(foodName != ''){
+                                    return <li>{foodName}</li>
+                                }
+                            })}
+                        </ul>
                     </div>
+                    <br />
                     <div class="invitations">
-                        <p>Invitations</p>
+                        <p>Invitations:</p>
+                        <ul>
+                            {invitations.map(invite => {
+                                console.log(invite[0])
+                                return <li>{invite}</li>
+                            })}
+                        </ul>
                     </div>
                 </div>
             </div>
--- bl-server/routes/users.js
+++ bl-server/routes/users.js
@@ -15,24 +15,24 @@ router.post('/register', async (req, res) => {
         if (password.length < 5) // checking if password is too short or not
             return res
                 .status(400)
-                .json({ msg: "Password must be at least 6 characters long"})
+                .json({ msg: "Password must be at least 6 characters long" })
         if (password !== passwordVerify)
             return res
                 .status(400)
                 .json({ msg: "Enter the same password twice for verification." });
-        const userEmailExists = await User.findOne({ email: email}) // searching if a user with this email exists
+        const userEmailExists = await User.findOne({ email: email }) // searching if a user with this email exists
         if (userEmailExists) // if user with this email exists, return error
             return res
                 .status(400)
-                .json({ msg: "An account with this email already exists"})
+                .json({ msg: "An account with this email already exists" })
         // findOne is a built-in mongoose feature for models, which allows you to search for different model instances
 
         console.log("HEY, ABOUT TO FIND USER")
-        const userUsernameExists = await User.findOne({ username: username}) // searching if a user with this username exists
+        const userUsernameExists = await User.findOne({ username: username }) // searching if a user with this username exists
         if (userUsernameExists) // if user with username exists, return error
             return res
                 .status(400)
-                .json({ msg: "An account with this username already exists"})
+                .json({ msg: "An account with this username already exists" })
         console.log("HEY")
         const salt = await bcrypt.genSalt(); // "salting" a password into a random string for extra security
         console.log("GENERATED SALT")
@@ -61,8 +61,8 @@ router.post('/login', async (req, res) => {
         console.log(email, "hi")
         if (!email || !password) // if not sent the username/password
             return res.status(400).json({ msg: "Not all fields have been entered" })
-        
-        const emailExists = await User.findOne({ email: email}); // searching through database for this email
+
+        const emailExists = await User.findOne({ email: email }); // searching through database for this email
         if (!emailExists) // If no user exists
             return res
                 .status(400)
@@ -71,8 +71,8 @@ router.post('/login', async (req, res) => {
 
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch)
-            return res.status(400).json({ msg: "Incorrect password or username"})
-        
+            return res.status(400).json({ msg: "Incorrect password or username" })
+
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET); // creating a token to represent the user's unique _id, which will be used in the future in authorization to verify that the user is the user.
         console.log("token: ", token);
         console.log("user: ", user);
@@ -91,118 +91,162 @@ router.post('/login', async (req, res) => {
 
 router.post("/tokenIsValid", async (req, res) => {
     try {
-      const token = req.header("x-auth-token");
-      if (!token) return res.json(false);
-  
-      const verified = jwt.verify(token, process.env.JWT_SECRET);
-      if (!verified) return res.json(false);
-  
-      const user = await User.findById(verified.id);
-      if (!user) return res.json(false);
-  
-      return res.json(true);
+        const token = req.header("x-auth-token");
+        if (!token) return res.json(false);
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        if (!verified) return res.json(false);
+
+        const user = await User.findById(verified.id);
+        if (!user) return res.json(false);
+
+        return res.json(true);
     } catch (err) {
-      res.status(500).json({ error: err.message });
+        res.status(500).json({ error: err.message });
     }
-  });
-  
-  router.get("/", auth, async (req, res) => {
+});
+
+router.get("/", auth, async (req, res) => {
     const user = await User.findById(req.user);
     res.json({
-      displayName: user.displayName,
-      id: user._id,
-      favFoods: user.favFoods,
+        displayName: user.displayName,
+        id: user._id,
+        favFoods: user.favFoods,
+        email: user.email,
+        username: user.username,
+        groups: user.groups,
+        invitations: user.invitations,
     });
-  });
+});
 
-  router.patch("/addFavFood", async (req, res) => {
-      try {
-          const user = await User.findById(req.body.id); // finding user in database
-          if (!user) {
-              return res.status(404).json({ message: 'Cannot find user' }) // when user doesn't exist...
-          }
-          res.user = user;
-          console.log("Before fav foods update", res.user.favFoods);
-          var favArr = req.body.putArr; // getting the new array of favorite foods from the request
-          if (req.body.putArr) {
-              res.user.favFoods = favArr; // if exists, set the favorite foods for the user in the data base to the new one (i.e. the new array with the new favorte food(s))
-          }
-          const updatedUser = await res.user.save();
-          console.log(res.user.favFoods);
-          res.json(updatedUser);
-      } catch (err) {
-          res.status(400).json({ error: err.message });
-      }
-  })
-
-  router.patch("/makeGroup", async (req, res) => { // group creation inside of users since a group must have at least one member in it; it's original creator
-      try {
+router.patch("/addFavFood", async (req, res) => {
+    try {
         const user = await User.findById(req.body.id); // finding user in database
-          if (!user) {
-              return res.status(404).json({ message: 'Cannot find user' }) // when user doesn't exist...
-          }
-          const group = await Group.findById(req.body.groupId)
-          res.user = user;
-          console.log("Before group created for user", res.user.groups);
-          var groupName = req.body.groupName;
-          var newGroupList = user.groups
-          if (groupName) {
-              user.groups = newGroupList; // adding new group name for user
-              groupMembers = [user.username]
-              const newGroup = new Group({
+        if (!user) {
+            return res.status(404).json({ message: 'Cannot find user' }) // when user doesn't exist...
+        }
+        res.user = user;
+        console.log("Before fav foods update", res.user.favFoods);
+        var favArr = req.body.putArr; // getting the new array of favorite foods from the request
+        if (req.body.putArr) {
+            res.user.favFoods = favArr; // if exists, set the favorite foods for the user in the data base to the new one (i.e. the new array with the new favorte food(s))
+        }
+        const updatedUser = await res.user.save();
+        console.log(res.user.favFoods);
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+})
+
+router.patch("/makeGroup", async (req, res) => { // group creation inside of users since a group must have at least one member in it; it's original creator
+    try {
+        const user = await User.findById(req.body.id); // finding user in database
+        if (!user) {
+            return res.status(404).json({ message: 'Cannot find user' }) // when user doesn't exist...
+        }
+        const group = await Group.findById(req.body.groupId)
+        res.user = user;
+        console.log("Before group created for user", res.user.groups);
+        var groupName = req.body.groupName;
+        var newGroupList = user.groups
+        if (groupName) {
+            user.groups = newGroupList; // adding new group name for user
+            groupMembers = [user.username]
+            const newGroup = new Group({
                 groupName,
                 groupMembers
-              })
-              const groupId = newGroup._id.toString()
-              console.log("groupId", groupId)
-              newGroupList.push({groupName, groupId}) // adding new group name to list of groups for the user
-              const savedGroup = await newGroup.save();
-              const updatedUser = await user.save();
-              res.json({savedGroup, updatedUser});
+            })
+            const groupId = newGroup._id.toString()
+            console.log("groupId", groupId)
+            newGroupList.push({ groupName, groupId }) // adding new group name to list of groups for the user
+            const savedGroup = await newGroup.save();
+            const updatedUser = await user.save();
+            res.json({ savedGroup, updatedUser });
             //   res.json(updatedUser);
-          }
-      } catch (err) {
+        }
+    } catch (err) {
         res.status(400).json({ error: err.message });
-      }
-  })
+    }
+})
 
-  router.patch("/addToGroup", async (req, res) => {
-      try {
-          console.log("USER ID", req.body.id)
-          console.log("GROUP ID", req.body.groupId)
+router.patch("/addToGroup", async (req, res) => {
+    try {
+        console.log("USER ID", req.body.id)
+        console.log("GROUP ID", req.body.groupId)
         const user = await User.findById(req.body.id); // finding user in database
-          if (!user) {
-              return res.status(404).json({ message: 'Cannot find user' }) // when user doesn't exist...
-          }
-          res.user = user;
-          console.log("Before user added to group", res.user.groups);
-          var groupName = req.body.groupName;
-          var newGroupList = user.groups
-          groupId = mongoose.Types.ObjectId(req.body.groupId)
-          console.log("NEW GROUP ID (OBJECT)", groupId)
-          var group = await Group.findById(groupId)
-          console.log("GROUP HERE", group)
-          if (group) {
-              user.groups = newGroupList; // adding new group name for user
-              groupMembers = group.groupMembers
-              console.log("GROUP MEMBERS", groupMembers)
-              console.log("USER'S USERNAME", user.username)
-              username = user.username
-              if (groupMembers.indexOf(user.username) !== -1) { // checking if user is already in the group...
+        if (!user) {
+            return res.status(404).json({ message: 'Cannot find user' }) // when user doesn't exist...
+        }
+        res.user = user;
+        console.log("Before user added to group", res.user.groups);
+        var groupName = req.body.groupName;
+        var newGroupList = user.groups
+        groupId = mongoose.Types.ObjectId(req.body.groupId)
+        console.log("NEW GROUP ID (OBJECT)", groupId)
+        var group = await Group.findById(groupId)
+        console.log("GROUP HERE", group)
+        if (group) {
+            user.groups = newGroupList; // adding new group name for user
+            groupMembers = group.groupMembers
+            console.log("GROUP MEMBERS", groupMembers)
+            console.log("USER'S USERNAME", user.username)
+            username = user.username
+            if (groupMembers.indexOf(user.username) !== -1) { // checking if user is already in the group...
                 console.log("IN THE IF STATEMENT")
                 return res.status(400).json("error, user already in group")
-              }
-              groupMembers.push(user.username) // adding new user to the exsting group
-              newGroupList.push({groupName, groupId}) // adding new group name to list of groups for the user
-              group.groupMembers = groupMembers
-              const savedGroup = await group.save();
-              const updatedUser = await user.save();
-              res.json({savedGroup, updatedUser});
+            }
+            groupMembers.push(user.username) // adding new user to the exsting group
+            newGroupList.push({ groupName, groupId }) // adding new group name to list of groups for the user
+            group.groupMembers = groupMembers
+            const savedGroup = await group.save();
+            const updatedUser = await user.save();
+            res.json({ savedGroup, updatedUser });
             //   res.json(updatedUser);
-          }
-      } catch (err) {
+        }
+    } catch (err) {
         res.status(400).json({ error: err.message });
-      }
-  })
+    }
+})
+
+router.post("/changePassword", async (req, res) => {
+    try {
+        console.log("a");
+        const { password, id } = req.body;
+        console.log(password, id);
+
+        console.log("b");
+        if (!password) // checkinf if all fields were submitted
+            return res.status(400).json({ msg: "Not all fields completed" })
+        if (password.length < 5) // checking if password is too short or not
+            return res
+                .status(400)
+                .json({ msg: "Password must be at least 6 characters long" })
+        
+        console.log("c");
+
+        const user = await User.findById(id);
+        if (!user)
+            return res
+                .status(400)
+                .json({ msg: "User not found" })
+
+        console.log("d");
+       res.user = user;
+       const salt = await bcrypt.genSalt(); // "salting" a password into a random string for extra security       
+       const passwordHash = await bcrypt.hash(password, salt);
+
+       console.log("e");
+       res.user.password = passwordHash;
+       const updatedUser = await res.user.save();       
+       res.json(updatedUser);
+       
+       console.log("f");
+    }
+    catch (err) {
+        res.status(505).json({ error: err.message });
+    }
+})
+
 
 module.exports = router;
