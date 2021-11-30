7c890a27a335329e49b62284fb8730011eedcda4
Matthew Yoshida <mchangyoshida@gmail.com>
2021-11-29 18:48:02 -0800

    Scraper now works on mac and execFile also runs the scraper bash scripts. Now all the stuff is in the js array of objects

--- bl-server/nutrition-facts/nutritionFactsScraper.js
+++ bl-server/nutrition-facts/nutritionFactsScraper.js
@@ -1,6 +1,21 @@
-//import fetch from 'node-fetch';
-(async () => {
-    const response = await fetch('https://menu.dining.ucla.edu/Menus/DeNeve');
-    const text = await response.text();
-    console.log(text.match(/?(<a class=\"recipelink\" href=').*(>)/));
-  })()
+const { execFileSync } = require('child_process');
+
+
+execFileSync('./nutrition_fact_scraper_bplate.sh');
+execFileSync('./nutrition_fact_scraper_deneve.sh');
+execFileSync('./nutrition_fact_scraper_epicuria.sh');
+execFileSync('./nutrition_fact_scraper_feast.sh');
+//console.log("hello");
+var jsonFileName = ['bplate_nutrient.json', 'deneve_nutrient.json','epicuria_nutrient.json', 'feast_nutrient.json']
+//var foodData[4]
+//var food [4]
+//var jsonFileName = './feast_nutrient.json'
+for (let i =0; i<4; i++){
+    const fs = require('fs');
+    let foodData=fs.readFileSync(jsonFileName[i]);
+    let foods = JSON.parse(foodData);
+    console.log("hello")
+    console.log(foods[1]);
+}
+
+
