01f31015ebb53d858b9dda7d8881aef58009fcc7
jaysonshinn <jayshinn@ucla.edu>
2021-11-29 21:20:37 -0800

    Revert "Merge pull request #27 from matthewcyy/checkout"
    This reverts commit 861e0263b93885105d49376318636fa6600ca077, reversing
    changes made to 9606b2e84d9ec3ca1a1e88815111d15840c8a1ce.

--- bl-client/src/Navbar.js
+++ bl-client/src/Navbar.js
@@ -50,11 +50,6 @@ function NavBar() {
                                 <ListItemText primary={<Box sx={{ color: '#f8b827', fontWeight: 'bold', fontSize:'1.25rem' }}>Rankings</Box>}/>
                             </ListItem>
                         </NavLink>
-                        <NavLink to="/voting" style={{textDecoration: 'none', marginRight: '0.5rem' }} disableRipple>
-                            <ListItem button>
-                                <ListItemText primary={<Box sx={{ color: '#f8b827', fontWeight: 'bold', fontSize:'1.25rem' }}>Votings</Box>}/>
-                            </ListItem>
-                        </NavLink>
                     </MenuItem>
                 </div>
                 <div>
