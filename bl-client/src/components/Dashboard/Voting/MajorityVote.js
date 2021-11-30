01042e5487e05878dcdd9d08e06af5ceef2856e4
Matthew Yoshida <mchangyoshida@gmail.com>
2021-11-29 18:50:09 -0800

    jayson jayson jayson jayson jayson done by jayson (also previous commit on main is by jayson) trying to map mangodb function, commiting before pulling. thanks, Jayson

--- bl-client/src/components/Dashboard/Voting/MajorityVote.js
+++ bl-client/src/components/Dashboard/Voting/MajorityVote.js
@@ -1,91 +1,38 @@
-import React, { Component, useContext } from 'react';
-import CanvasJSReact from '../../assets/canvasjs.react';
-import axios from 'axios';
-// import UserContext from '../../../context/userContext';
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+import React, { useEffect, useRef } from 'react';
+import Chartjs from 'chart.js';
+import axos from 'axios'
 
-var updateInterval = 500;
+const Chart = () => {
+  const chartContainer = useRef(null);
+  const [chartInstance, setChartInstance] = useState(null);
 
-class Voting extends Component {
-	constructor(props) {
-		super(props);
-		this.updateChart = this.updateChart.bind(this);
-	}
-	componentDidMount(){
-		setInterval(this.updateChart, updateInterval);
-	}
-	updateChart() { //fetch data from mongodb 
-		var dpsTotal = 0, yVal;
-		var dps = this.chart.options.data[0].dataPoints;
-		
-		this.chart.options.data[0].dataPoints = dps;
-		this.chart.render();
-	}
-	render() {
-		if ()//check if logged in
-			{
-				const options = {
-					theme: "dark2",
-					title: {
-						text: "Dining Hall Majority Vote"
-					},
-					subtitles: [{
-						text: "Where are we going to eat?"
-					}],
-					axisY: {
-						title: "Total Votes",
-						includeZero = True,
-					maximum: 100
-					},
-					data: [{
-						type: "column",
-						yValueFormatString: "#,###'%'",
-						indexLabel: "{y}",
-						dataPoints: [
-							{ label: "Epicuria", y: 0 },
-							{ label: "bPlate", y: 0 },
-							{ label: "De Neve", y: 0 },
-							{ label: "Feast", y: 0}
-						]
-					}]
-				}
-			}
-			else
-			{
+  const chartConfig = {
+    type: 'bar',
+    data: {
+      labels: ['DeNeve', 'Epicuria', 'B-Plate', 'Feast']
+      [2, 3, 4, 5]
+    },
+    options: {
+      scales: {
+        y: {
+            beginAtZero: true
+        }
+    }
+    }
+  };
 
-			}
-		
-		return ( //buttons here will send data to group mongodb 
-			<>
-				<div>
-					<button onClick={this.updateChart}> 
-						Epicuria
-					</button>
-				</div>			
-				<div>
-					<button onClick={this.updateChart}>
-						bPlate
-					</button>
-				</div>
-				<div>
-					<button onClick={this.updateChart}>
-						De Neve
-					</button>
-				</div>
-				<div>
-					<button onClick={this.updateChart}>
-						Feast
-					</button>
-				</div>
-				<div>
-					<CanvasJSChart options = {options}
-						 onRef={ref => this.chart = ref}
-					/>
-					{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-				</div> 
-			</>
-		);
-	}
-}
+  useEffect(() => {
+    if (chartContainer && chartContainer.current) {
+      const newChartInstance = new Chartjs(chartContainer.current, chartConfig);
+      setChartInstance(newChartInstance);
+    }
+  }, [chartContainer]);
 
-export default Voting;
+  return (
+    <div>
+      <canvas ref={chartContainer} />
+    </div>
+  );
+};
+
+export default Chart;
