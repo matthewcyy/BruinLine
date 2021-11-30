e66ad3e4beb2a2c70a9547b4209569a84093be53
jaysonshinn <jayshinn@ucla.edu>
2021-11-30 12:50:01 -0800

    the current nutrition-facts in master

---
+++ bl-server/nutrition-facts/nutritionFactsScraper.js
@@ -0,0 +1,24 @@
+const { execFileSync } = require('child_process');
+const axios = require ('axios');
+
+// execFileSync('./nutrition_fact_scraper_bplate.sh');
+// execFileSync('./nutrition_fact_scraper_deneve.sh');
+// execFileSync('./nutrition_fact_scraper_epicuria.sh');
+// execFileSync('./nutrition_fact_scraper_feast.sh');
+//console.log("hello");
+var jsonFileName = ['bplate_nutrient.json', 'deneve_nutrient.json','epicuria_nutrient.json', 'feast_nutrient.json']
+//var foodData[4]
+//var food [4]
+//var jsonFileName = './feast_nutrient.json'
+for (let i =0; i<4; i++){
+    const fs = require('fs');
+    let foodData=fs.readFileSync(jsonFileName[i]);
+    let foods = JSON.parse(foodData);
+    console.log("hello")
+    console.log(foods[1]);
+    foods.map(insertFood)
+}
+async function insertFood(food){
+    //console.log(food);
+    const addFoodItem = await axios.post("http://localhost:5000/foods/addFood", food);
+}
--- bl-server/nutrition-facts/nutrition_fact_scraper_bplate.sh
+++ bl-server/nutrition-facts/nutrition_fact_scraper_bplate.sh
@@ -1,76 +1,62 @@
+#!/bin/bash
 website="https://menu.dining.ucla.edu/Menus/BruinPlate"
-P=$(curl $website)
+P=$(curl -s $website|perl -Mopen=locale -pe 's/&#x([\da-f]+);/chr hex $1/gie')
 echo "$P"|grep '<a class=\"recipelink\" href='|awk -F '"' '{print $4}'>urls.txt
 #echo \n
 echo "$P"|grep '<a class=\"recipelink\" href='|awk -F '[<>]' '{print $3}'>names.txt
 dining_hall="BPlate"
 file_name_json="bplate_nutrient.json"
+#echo names.txt| perl -Mopen=locale -pe 's/&#x([\da-f]+);/chr hex $1/gie'>names.txt
 
 #paste -d "\n" names.txt urls.txt > combined
 #echo $N
 echo "" > $file_name_json;
 
-curl "$website/Tomorrow"|grep '<a class=\"recipelink\" href='|awk -F '"' '{print $4}'>>urls.txt
-curl "$website/Tomorrow"|grep '<a class=\"recipelink\" href='|awk -F '[<>]' '{print $3}'>>names.txt
-
+curl -s "$website/Tomorrow"|grep '<a class=\"recipelink\" href='|awk -F '"' '{print $4}'>>urls.txt
+curl -s "$website/Tomorrow"|grep '<a class=\"recipelink\" href='|awk -F '[<>]' '{print $3}'>>names.txt
+DATE=$(date)
 for i in {2..8}
 do
-    NEXT_DATE=$(date +%Y-%m-%d -d "$DATE + $i day")
-    echo "$NEXT_DATE"
-    #P=$(curl "$website$NEXT_DATE)
-    curl "$website/$NEXT_DATE"|grep '<a class=\"recipelink\" href='|awk -F '"' '{print $4}'>>urls.txt
-    curl "$website/$NEXT_DATE"|grep '<a class=\"recipelink\" href='|awk -F '[<>]' '{print $3}'>>names.txt
+    #NEXT_DATE=$(date +%Y-%m-%d -d "$DATE + $i day")
+    NEXT_DATE=$(date -v+"$i"d "+%Y-%m-%d")
+    echo "$NEXT_DATE">>nextday.txt
+    #P=$(curl -s "$website$NEXT_DATE)
+    curl -s "$website/$NEXT_DATE"|grep '<a class=\"recipelink\" href='|awk -F '"' '{print $4}'>>urls.txt
+    curl -s "$website/$NEXT_DATE"|grep '<a class=\"recipelink\" href='|awk -F '[<>]' '{print $3}'>>names.txt
 
 done
 
 N=$(cat names.txt|wc -l)
 
-# done 3<./names.txt 4<./urls.txt
-# while read p < $4 && while read q < $3;
-# do
-#   echo $combined
-#   #>> nutritionfacts.txt
-#   echo $p
-#   curl $p| grep "nfserv" >> nutritionfacts.txt
-#   curl $p| grep "nfcal" >> nutritionfacts.txt
-#   curl $p| grep "nfnutrient" >> nutritionqfacts.txt
-#   echo "" >> nutritionfacts.txt
-# done 3<./names.txt 4<./urls.txt
+#paste -d"|" names.txt urls.txt | sort | uniq >combined.txt
 
-paste -d"|" names.txt urls.txt | sort | uniq >combined.txt
-i=0
 echo "[" >>$file_name_json
 paste -d "|" names.txt urls.txt |sort|uniq| while IFS="|" read -r q p
 do
     #%printf 'f1: %s\n' "$q"ss
     #echo $p
-    P=$(curl $p)
+    P=$(curl -s $p)
     #echo $P
     echo "{">>$file_name_json
-    echo -e "\t\"itemName\": \"$q\"," |sed -e 's/[\t][\t]*",$/",/'>>$file_name_json
-    echo -e "\t\"diningHall\": \"$dining_hall\"," >>$file_name_json
-    echo "$P"|grep "<p class=\"nfcal\"><span class=\"nfcaltxt\">Calories</span>" | sed -e 's/[<][^>]*[>]//g; s/[ \t]*Calories/\t"calories":/g'|sed 's/.$/,/' >> $file_name_json
-    echo "$P"|grep "<p class=\"nfnutrient\"><span class=\"nfmajornutrient\">Total Fat</span>" | sed -e 's/[<][^>]*[>]//g; s/[ \t]*Total Fat/\t"fat":/g; s/g[ ][0-9]*%/,/g' >> $file_name_json
-    echo "$P"|grep "<p class=\"nfnutrient\"><span class=\"nfmajornutrient\">Total Carbohydrate</span>" | sed -e 's/[<][^>]*[>]//g; s/[ \t]*Total Carbohydrate/\t"carbs":/g; s/g[ ][0-9]*%/,/g'| >> $file_name_json
-    echo "$P"|grep "<p class=\"nfnutrient\"><span class=\"nfmajornutrient\">Protein</span>" |sed -e 's/[<][^>]*[>]//g; s/[ \t]*Protein/\t"protein":/g; s/g//g' >> $file_name_json
+    echo "\"itemName\": \"$(echo $q|sed 's/"//g')\"," |sed -E 's/\"itemName\"/\t\"itemName\"/'>>$file_name_json
+    echo "\"diningHall\": \"$dining_hall\","|sed -e 's/\"diningHall\"/\t\"diningHall\"/' >>$file_name_json
+    echo "$P"|grep "<p class=\"nfcal\"><span class=\"nfcaltxt\">Calories</span>" | sed -E 's/[<][^>]*[>]//g; s/[ \t]*Calories/\t"calories":/g'|sed 's/.$/,/' >> $file_name_json
+    echo "$P"|grep "<p class=\"nfnutrient\"><span class=\"nfmajornutrient\">Total Fat</span>" | sed -E 's/[<][^>]*[>]//g; s/[ \t]*Total Fat/"fat":/g; s/g[ ][0-9]*%/,/g' >> $file_name_json
+    echo "$P"|grep "<p class=\"nfnutrient\"><span class=\"nfmajornutrient\">Total Carbohydrate</span>" | sed -E 's/[<][^>]*[>]//g; s/[ \t]*Total Carbohydrate/"carbs":/g; s/g[ ][0-9]*%/,/g' >> $file_name_json
+    echo "$P"|grep "<p class=\"nfnutrient\"><span class=\"nfmajornutrient\">Protein</span>" |sed -E 's/[<][^>]*[>]//g; s/[ \t]*Protein/"protein":/g; s/g//g' >> $file_name_json
     if echo "$P"|grep "<span class=\"nfcaltxt\">Calories"
     then 
         echo
     else  
-        echo -e "\t\"calories\": \"N/A\",">> $file_name_json
-        echo -e  "\t\"fat\": \"N/A\",">> $file_name_json
-        echo -e "\t\"carbs\": \"N/A\",">> $file_name_json
-        echo -e "\t\"protein\": \"N/A\"">> $file_name_json
+        echo  "\"calories\": \"N/A\"," |sed -E 's/\"calories\"/\t\"calories\"/'>> $file_name_json
+        echo  "\"fat\": \"N/A\"," |sed -E 's/\"fat\"/\t\"fat\"/'>> $file_name_json
+        echo "\"carbs\": \"N/A\"," |sed -E 's/\"carbs\"/\t\"carbs\"/'>> $file_name_json
+        echo "\"protein\": \"N/A\"" |sed -E 's/\"protein\"/\t\"protein\"/'>> $file_name_json
     fi
-    if [ i -eq $N-1 ];
-    then
-        echo "}">>$file_name_json
-    else
-        echo "},">>$file_name_json
-    fi
-    ((i++))
+    echo "},">>$file_name_json
+
 done
-sed -i '$ s/,$//g' $file_name_json
+sed -i '' '$ s/,$//g' $file_name_json
 echo "]">>$file_name_json
 #rm $file_name_json
 #cp nutritionfacts.txt $file_name_json
