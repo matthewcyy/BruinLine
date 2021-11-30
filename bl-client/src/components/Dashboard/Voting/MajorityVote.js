92041c0dd181378c1df723c3912820dbd9b813ce
newtyee18 <92614955+newtyee18@users.noreply.github.com>
2021-11-29 20:21:45 -0800

    completed group voting

--- bl-client/src/components/Dashboard/Voting/MajorityVote.js
+++ bl-client/src/components/Dashboard/Voting/MajorityVote.js
@@ -1,10 +1,49 @@
-import React, { Component, useContext } from 'react';
+import React, { Component } from 'react';
 import CanvasJSReact from '../../../assets/canvasjs.react';
 import axios from 'axios';
+import Button from '@mui/material/Button';
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 var CanvasJS = CanvasJSReact.CanvasJS;
 
-class Voting extends Component {
+class Voting extends Component {	
+	constructor(props) {
+		super(props);
+	}
+
+
+	async makeVote(diningHall) {
+		if (diningHall === "B-Plate") {
+			diningHall = "bPlate"
+		}
+		if (diningHall === "De Neve") {
+			diningHall = "DeNeve"
+		}
+		this.props.changeVote(diningHall)
+		var reqBody = {}
+		reqBody.groupId = this.props.groupId
+		reqBody.diningHall = diningHall
+		const changeVote = await axios.patch('http://localhost:5000/groups/vote', reqBody)
+		localStorage.setItem("vote", diningHall)
+		this.props.setDisable(true)
+	}
+
+	async removeVote() {
+		var diningHall = localStorage.getItem("vote")
+		if (diningHall === "B-Plate") {
+			diningHall = "bPlate"
+		}
+		if (diningHall === "De Neve") {
+			diningHall = 'DeNeve'
+		}
+		this.props.removeVote(diningHall)
+		var reqBody={}
+		var reqBody = {}
+		reqBody.groupId = this.props.groupId
+		reqBody.diningHall = diningHall
+		const changeVote = await axios.patch('http://localhost:5000/groups/removeVote', reqBody)
+		localStorage.setItem("vote", "")
+		this.props.setDisable(false)
+	}
 	addSymbols(e){
 		var suffixes = ["", "K", "M", "B"];
 		var order = Math.max(Math.floor(Math.log(e.value) / Math.log(1000)), 0);
@@ -13,38 +52,41 @@ class Voting extends Component {
 		var suffix = suffixes[order];
 		return CanvasJS.formatNumber(e.value / Math.pow(1000, order)) + suffix;
 	}
+
 	render() {
+	console.log("PROPS", this.props)
 		const options = {
 			theme: "light2",
-			title:{
-				text: "Dining Hall Majority Vote"
-			},
 			axisX: {
 				title: "Dining Hall",
-				reversed: true,
 			},
 			axisY: {
 				title: "Number of Votes",
+				includeZero: true,
 				labelFormatter: this.addSymbols
 			},
 			data: [{
 				type: "bar",
 				dataPoints: [
-					{ y:  4, label: "B-Plate" },
-					{ y:  5, label: "Feast" },
-					{ y:  7, label: "De Neve" },
-					{ y:  6, label: "Epicuria" }
+					{ y:  this.props.votes.bPlate, label: "B-Plate" },
+					{ y:  this.props.votes.Feast, label: "Feast" },
+					{ y:  this.props.votes.DeNeve, label: "De Neve" },
+					{ y:  this.props.votes.Epicuria, label: "Epicuria" }
 				]
 			}]
 		}
-		
+
 		return (
 		<div>
-			<h1>Vote Which Dining Hall You Want To Eat At!</h1>
 			<CanvasJSChart options = {options} 
 				/* onRef={ref => this.chart = ref} */
 			/>
 			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+			<Button disabled={this.props.undoDisable} onClick={()=>this.makeVote("B-Plate")}>B-Plate</Button>
+			<Button disabled={this.props.undoDisable} onClick={()=>this.makeVote("Feast")}>Feast</Button>
+			<Button disabled={this.props.undoDisable} onClick={()=>this.makeVote("De Neve")}>De Neve</Button>
+			<Button disabled={this.props.undoDisable} onClick={()=>this.makeVote("Epicuria")}>Epicuria</Button>
+			<Button disabled={!this.props.undoDisable} onClick={()=>this.removeVote()}>Undo Vote</Button>
 		</div>
 		);
 	}
